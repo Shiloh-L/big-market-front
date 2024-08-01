3933d96d669bbc4d9d771b990ca4da377425fb98
Shiloh <shilliu@cisco.com>
2024-08-01 17:22:22 +0800

    feat: 对接后端接口

--- src/apis/index.tsx
+++ src/apis/index.tsx
@@ -1,17 +1,34 @@
 // 请求地址
-const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "https://mock.apipost.net/mock/2f628dd6b464000";
+const apiHostUrl = process.env.API_HOST_URL ? process.env.API_HOST_URL : "";
+
+/**
+ * 装配抽奖
+ * @param strategyId
+ */
+export const strategyArmory = (strategyId?: number) => {
+    return fetch(`${apiHostUrl}/api/v1/raffle/strategy_armory?strategyId=${strategyId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
+
 
 /**
  * 查询抽奖奖品列表
  * @param strategyId 策略ID
  */
-export const queryRaffleAwardList = (strategyId: number) => {
+export const queryRaffleAwardList = (strategyId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list?strategyId=${strategyId}&apipost_id=362969d03c4001`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/query_raffle_award_list`, {
+            method: 'POST',
             headers: {
-                'Content-Type': 'application/json;charset=utf-8'
-            }
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
         });
     } catch (error) {
         return fetch("{\n" +
@@ -36,13 +53,17 @@ export const queryRaffleAwardList = (strategyId: number) => {
  * 	}
  * }
  */
-export const randomRaffle = (strategyId: number) => {
+export const randomRaffle = (strategyId?: number) => {
     try {
-        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle?strategyId=${strategyId}&apipost_id=362b2ed77c405d`, {
-            method: 'get',
+        return fetch(`${apiHostUrl}/api/v1/raffle/random_raffle`, {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
-            }
+            },
+            body: JSON.stringify({
+                strategyId: strategyId
+            })
+
         })
     } catch (error) {
         return fetch("{\n" +
