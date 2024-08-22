c279d0143f0700d209ef92912720fba5621e5ac2
Shiloh <shilliu@cisco.com>
2024-08-22 16:04:19 +0800

    feat: 活动信息API迭代和功能完善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,10 @@ const StrategyArmoryButton = dynamic(async () => (await import("./components/Str
 export default function Home() {
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100"
+             style={{backgroundImage: "url('/background.svg')", backgroundColor: "#e7305e;"}}>
             {/* 头部文案 */}
-            <header className="text-3xl font-bold text-center text-gray-800 my-8">
+            <header className="text-7xl font-bold text-center text-gray-800 my-8" style={{color: "white"}}>
                 大营销平台 - 抽奖展示
             </header>
 
@@ -33,8 +34,9 @@ export default function Home() {
             </div>
 
             {/* 底部文案 */}
-            <footer className="text-gray-600 text-center my-8">
-                DDD项目实战，欢迎访问我的博客：https://blog.shiloh98.top:9443
+            <footer className="text-gray-600 text-center my-8" style={{color: "white"}}>
+                DDD项目实战，欢迎访问我的博客：<a href='https://blog.shiloh98.top:9443'
+                                                target='_blank'>https://blog.shiloh98.top:9443</a> @Shiloh
             </footer>
         </div>
     );
